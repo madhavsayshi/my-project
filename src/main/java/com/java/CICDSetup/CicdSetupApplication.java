d638a9938d61397a37140575925a1d6b77b9bbf0
madhavsayshi <madhavsayshi@gmail.com>
2025-01-29 22:45:18 +0100

    new project setup

---
+++ src/main/java/com/java/CICDSetup/CicdSetupApplication.java
@@ -0,0 +1,13 @@
+package com.java.CICDSetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdSetupApplication.class, args);
+	}
+
+}
